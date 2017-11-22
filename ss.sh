509269fcb33f9261c49daad3a0d5078857b76dd4
drice82 <xujiabin82@gmail.com>
2017-11-22 20:20:31 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -3,14 +3,18 @@ apt-get update
 apt-get install python-pip git python-m2crypto supervisor vnstat lftp fail2ban wget 
 pip install cymysql
 cd /
-git clone -b manyuser https://github.com/drice82/shadowsocks.git
-cd /shadowsocks
-echo "[program:shadowsocks]" > /etc/supervisor/conf.d/shadowsocks.conf
-echo "command=python /shadowsocks/server.py" >> /etc/supervisor/conf.d/shadowsocks.conf
-echo "autorestart=true" >> /etc/supervisor/conf.d/shadowsocks.conf
-echo "user=root" >> /etc/supervisor/conf.d/shadowsocks.conf
 
-nano /shadowsocks/usermysql.json
+#git clone -b manyuser https://github.com/drice82/shadowsocks.git
+git clone https://github.com/drice82/shadowsocksr
+cd /shadowsocksr
+bash /shadowsocksr/initcfg.sh
+bash /shadowsocksr/setup_cymysql.sh
+echo "[program:shadowsocksr]" > /etc/supervisor/conf.d/shadowsocksr.conf
+echo "command=python /shadowsocksr/server.py" >> /etc/supervisor/conf.d/shadowsocksr.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/shadowsocksr.conf
+echo "user=root" >> /etc/supervisor/conf.d/shadowsocksr.conf
+
+nano /shadowsocksr/usermysql.json
 
 service supervisor start 
 supervisorctl reload
